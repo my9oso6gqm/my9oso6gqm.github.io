28e085772a68512dffa46db798968a8ed93cc850
mrzgrd <mrzgrd@localhost>
2021-04-05 15:49:30 +0200

    my9oso6gqm

---
+++ varie/js/menu/menu.js
@@ -0,0 +1,2 @@
+ 
+document.getElementById("menu").innerHTML = '<a href="/index.html">Pagina d\'entrata</a> - <a href="/blog/index.html">Blog</a> <a href="/blog/blog.rss">(RSS)</a> - <a href="/varie.html">Varie</a> - <a href="/contatti.html">Contatti</a>';
