d9a76cbc87d6b2583abaca0bd678f56a421ba430
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2021-11-21 00:54:33 +0100

    Update menu.js

--- varie/js/menu/menu.js
+++ varie/js/menu/menu.js
@@ -1,1 +1,1 @@
- document.getElementById("menu").innerHTML = '<a href="/index.html">Pagina d\'entrata</a> - <a href="/blog/index.html">Blog</a> <a href="/blog/blog.rss">(RSS)</a> - <a href="/varie.html">Varie</a> - <a href="/contatti.html">Contatti</a>';
+ document.getElementById("menu").innerHTML = '<a href="/index.html">Pagina d\'entrata</a> - <a href="/blog/">Blog</a> <a href="/blog/blog.rss">(RSS)</a> - <a href="/varie/">Varie</a> - <a href="/contatti.html">Contatti</a>';
