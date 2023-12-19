ad52112d9eed00253def3b00f823c1706d522949
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-19 16:52:04 +0100

    Create 191220231640.py

---
+++ tmp/191220231640.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.markdown(r"""
+
+# Ibkr api ufficiali
+
+Sono temute per essere ostiche e in effetti hanno migliaia, letteralmente migliaia di parametri possibili. La documentazione è completa ma manca qualcosa per iniziare e ci sta solo un sito terzo con un esempio, che però ho scoperto essere ripreso dal blog di ib. Questo codice invece viene dal canale youtube ufficiale di ib.
+
+!(https://my9oso6gqm.github.io/tmp/191220231646.jpg)
+
+Ora che riesco a interrogare ib direttamente e ne ho compreso la logica delle sue api senza ripiegare a lib terze posso buttarmi sulla cosa davvero interessante che è pensare e scrivere algoritmi.
+
+""")
